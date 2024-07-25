8695503d86d2bd6019b6f8773673cfc0343a1bcd
AbhyudayadityaKumarSingh <abhyudayaditya.219302471@muj.manipal.edu>
2024-07-25 21:48:01 +0530

    commit

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React ,{useState,useEffect}from 'react'
+import toast from 'react-hot-toast'
 import Layout from '../../components/Layout/Layout'
 import AdminMenu from '../../components/Layout/AdminMenu'
+import axios from 'axios'
+import { get } from 'mongoose'
 
 const CreateCategory = () => {
+  const [categories,setCategories] = useState([]);
+
+  //get all categories
+  const getAllCategories = async () => {
+    try {
+     const { data } = await axios.get(`/api/v1/category/categories`);
+
+      if(data.success){
+        setCategories(data.categories);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Error Loading Categories');
+    }
+  };
+
+  useEffect(() => {
+     getAllCategories();
+  }, []);
   return (
     
     <Layout title={'Dashboard - Create Category'}>
@@ -13,7 +35,27 @@ const CreateCategory = () => {
 
     </div>
     <div className='col-md-9'>
-     <h1>Create  Category</h1>
+     <h1>Create and Manage Categories</h1>
+    </div>
+    <div>
+<table className="table">
+  <thead>
+    <tr>
+     
+      <th scope="col">Name</th>
+      <th scope="col">Actions</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+    <tr>
+   
+     
+    </tr>
+  </tbody>
+</table>
+
     </div>
 
     </div>
