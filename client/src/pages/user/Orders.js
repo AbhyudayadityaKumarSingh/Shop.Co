52c5b71349be8e45bb63dd6410b61b8c5fe33314
AbhyudayadityaKumarSingh <abhyudayaditya.219302471@muj.manipal.edu>
2024-07-27 11:03:52 +0530

    made orders

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -27,6 +27,36 @@ const Orders = () => {
     </div>
        <div className='col-md-9'>
         <h1>All Orders</h1>
+            {
+              orders?Map((o,i)=> {
+                return (
+                  <div key={i} className='card m-3'>
+                    <div className='card-header'>
+                      <h5>Order ID : {o._id}</h5>
+                    </div>
+                    <div className='card-body'>
+                      <h5>Products</h5>
+                      <hr />
+                      {
+                        o.products.map((p,i)=> {
+                          return (
+                            <div key={i}>
+                              <h6>{p.name}</h6>
+                              <h6>Price : {p.price}</h6>
+                              <h6>Quantity : {p.count}</h6>
+                            </div>
+                          )
+                        })
+                      }
+                      <h6>Total Amount : {o.amount}</h6>
+                      <h6>Order Date : {o.createdAt}</h6>
+                    </div>
+                  </div>
+                )
+
+              })
+              : <h1>No Orders</h1>
+            }
        </div>
     </div>
     </div>
